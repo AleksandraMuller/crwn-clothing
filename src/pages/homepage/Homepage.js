9fac2bc9cb23a41fe891466172852fb655b8b830
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-04-01 17:59:20 +0200

    broken homepage into smaller components and finished styling menu-item

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Directory } from "./../../components/directory/Directory";
+
+import "./homepage.styles.scss";
+
+export const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
