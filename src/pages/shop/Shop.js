327c20ed64487c079ee87c026cfd45a544f2660a
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-04-15 01:35:43 +0200

    redux-thunk, refactored shop reducer

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,57 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route } from "react-router-dom";
-
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
+import { createStructuredSelector } from "reselect";
 
 import { CollectionsOverview } from "../../components/collections-overview/CollectionsOverview";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import {
+  selectIsCollectionFetching,
+  selectIsCollectionsLoaded
+} from "../../redux/shop/shop.selectors";
 import { WithSpinner } from "../../components/with-spinner/WithSpinner";
 import { Collection } from "../collection/Collection";
-// import { Category } from "../category/Category";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionWithSpinner = WithSpinner(Collection);
 
 export const Shop = ({ match }) => {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
   const unsubscribeFromSnapshot = null;
+  const dispatch = useDispatch();
+
+  const selectors = useSelector(
+    createStructuredSelector({
+      isCollectionFetching: selectIsCollectionFetching,
+      isCollectionLoaded: selectIsCollectionsLoaded
+    })
+  );
 
   useEffect(() => {
-    const collectionRef = firestore.collection("collection");
-
-    collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionsMap);
-      dispatch(updateCollections(collectionsMap));
-      setLoading(false);
-    });
-
-    // fetch(
-    //   "https://firestore.googleapis.com/v1/projects/crwn-db/databases/(default)/documents/collections"
-    // )
-    //   .then(response => response.json())
-    //   .then(collections => console.log(collections));
+    dispatch(fetchCollectionsStartAsync());
   }, [dispatch]);
+
   return (
     <div className="shop-page">
       <Route
         exact
         path={`${match.path}`}
-        // component={CollectionsOverview}
         render={props => (
-          <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          <CollectionsOverviewWithSpinner
+            isLoading={selectors.isCollectionFetching}
+            {...props}
+          />
         )}
       />
       <Route
         path="/shop/:collectionId"
-        // component={Collection}
         render={props => (
-          <CollectionWithSpinner isLoading={loading} {...props} />
+          <CollectionWithSpinner
+            isLoading={!selectors.isCollectionLoaded}
+            {...props}
+          />
         )}
       />
       {/* <Route path={`${match.path}/:categoryId`} component={Category} /> */}
